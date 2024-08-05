b1d6c70d5617099ae8c1803f0ae760018d78cb21
sairam751 <sairam751348@gmail.com>
2024-08-05 20:13:25 +0530

    MLFlow Basics

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MLFlowProject
+
+https://dagshub.com/sairam751/MLFlowProject.mlflow
+
+import dagshub
+dagshub.init(repo_owner='sairam751', repo_name='MLFlowProject', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- app.py
+++ app.py
@@ -5,6 +5,9 @@
 import os
 import warnings
 import sys
+import dagshub
+dagshub.init(repo_owner='sairam751', repo_name='MLFlowProject', mlflow=True)
+
 
 import pandas as pd
 import numpy as np
@@ -80,7 +83,7 @@ if __name__ == "__main__":
 
         ## For Remote server only(DAGShub)
 
-        remote_server_uri="https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
+        remote_server_uri="https://dagshub.com/sairam751/MLFlowProject.mlflow"
         mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
